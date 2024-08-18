d3f49b8b4e0a25744c177d408139d3d5e5030203
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-08-18 20:54:56 +0530

    Updating The UI + Adding Project Section

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let burgerIcon = document.querySelector(".img1");
 let burgerIconImg = document.querySelector(".hmImg");
 let burgerContent = document.querySelector(".hmbgr-cont");
 
+
+
 (() => {
   burgerIconImg.src = "assets/hambj-horiz.avif";
 })();
@@ -16,16 +18,32 @@ burgerIcon.addEventListener("click", () => {
   }
 });
 
+
+
+// const getRandColor = () => {
+//   let R = Math.floor(Math.random() * 255);
+//   let G = Math.floor(Math.random() * 255);
+//   let B = Math.floor(Math.random() * 255);
+//   return `rgb(${R},${G},${B})`;
+// };
+// let alphabet = document.querySelectorAll(".TEXT");
+// alphabet.forEach((color) => {
+//   color.style.color = getRandColor();
+// });
+
 const getRandColor = () => {
-  let R = Math.floor(Math.random() * 255);
-  let G = Math.floor(Math.random() * 255);
-  let B = Math.floor(Math.random() * 255);
+  // Set a minimum value for R, G, B to ensure bright colors
+  const min = 150;
+  const max = 255;
+  
+  let R = Math.floor(Math.random() * (max - min + 1)) + min;
+  let G = Math.floor(Math.random() * (max - min + 1)) + min;
+  let B = Math.floor(Math.random() * (max - min + 1)) + min;
+
   return `rgb(${R},${G},${B})`;
 };
-let colorrr = document.querySelectorAll(".paras");
-// code to change color of each paras to a different color
-colorrr.forEach((color) => {
+
+let alphabet = document.querySelectorAll(".TEXT");
+alphabet.forEach((color) => {
   color.style.color = getRandColor();
 });
-
-
